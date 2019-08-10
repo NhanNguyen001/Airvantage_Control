10fdd6a00d57df3515fe7757a3a09b420aba373d
NatBach <51453941+NatBach@users.noreply.github.com>
2019-08-10 19:45:42 +0700

    Add files via upload

--- Landing.js
+++ Landing.js
@@ -6,12 +6,12 @@ const Landing = () => {
 		<section className="landing">
 			<div className="dark-overlay">
 				<div className="landing-inner">
-						<h1 className="x-large">Sierra Wireless</h1>
+						<h1 className="x-large">Where the internet speed is mistakenly as the light speed.</h1>
 						<p className="lead">
 							Congrats you have come to the right place for the right thing!!
 						</p>
 						<div className="buttons">
-							<Link to="/register" className="btn btn-primary">Sign Up</Link>
+							<Link to="/register" className="signupButton">Sign Up</Link>
 							<Link to="/login" className="btn btn-light">Login</Link>
 						</div>
 				</div>
