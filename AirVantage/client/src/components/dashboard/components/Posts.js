ac9a7e2273a5cd23df9c4cc2aeb8497edaab17bb
NatBach <51453941+NatBach@users.noreply.github.com>
2019-08-14 17:50:51 +0700

    Add files via upload

--- AirVantage/client/src/components/dashboard/components/Posts.js
+++ AirVantage/client/src/components/dashboard/components/Posts.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
 
 import Search from './Search';
+import { deleteProfile } from '../../../actions/module';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './Posts.css';
 
@@ -18,42 +23,46 @@ class Posts extends Component {
     this.setState({ searchField: e.target.value })
   }
 
-
-  render() {
-    const { posts } = this.props;
+ 
+  render(){
+    const { posts, deleteProfile } = this.props;
     const { searchField } = this.state;
-    const filteredMonsters =
+    const filteredMonsters = 
       posts.filter(post => post.name.toLowerCase().includes(searchField.toLowerCase()))
-        .map((post, index) => {
-          const { name, imei, macaddress, serialnumber, remove } = post
-          return (
-            <tr key={index} className="row100 body">
-              <td className="cell100 column1">{name}</td>
-              <td className="cell100 column2">{imei}</td>
-              <td className="cell100 column3">{macaddress}</td>
-              <td className="cell100 column4">{serialnumber}</td>
-              <td className="cell100 column5">{remove} <i class="fas fa-times"></i></td>
-            </tr>
-          )
-        });
+            .map((post, index) => {
+              const { name, imei, macaddress, serialnumber, _id } = post
+              return (
+                <tr key={index} className="row100 body">
+                  <td className="cell100 column1">{name}</td>
+                  <td className="cell100 column2">{imei}</td>
+                  <td className="cell100 column3">{macaddress}</td>
+                  <td className="cell100 column4">{serialnumber}</td>
+                  <td className="cell100 column5" class="removeButton" onClick={() => deleteProfile(_id)}>
+                    {/* <i 
+                      className="fas fa-times"
+                      // class="removeButton"
+                      onClick={() => deleteProfile(_id)}
+                      ></i> */}
+                  </td>
+                </tr>
+                )
+              });       
     return (
       <div className="limiter">
         <div>
           <h2>Information Board</h2>
         </div>
         <div class="d-flex flex-row justify-content-between">
-          <Search
+          <Search 
             placeholder="Search Profile"
             handleChange={this.handleChange}
           />
-
-          <Link
-            class="addButton"
-            type="button"
-            // className="addButton"
-            // className="addButton"
+          
+          <Link 
+            input="button" 
+            className="addButton"
             to='/create-module'>
-            Add</Link>
+              <img class='addUser' src='https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-person-add-512.png'></img> </Link>
 
         </div>
         <div className="container-table100">
@@ -63,10 +72,10 @@ class Posts extends Component {
                 <table>
                   <thead>
                     <tr className="row100 head">
-                      <th className="cell100 column1">Name</th>
+                    <th className="cell100 column1">Name</th>
                       <th className="cell100 column2">IMEI</th>
                       <th className="cell100 column3">MAC Address</th>
-                      <th className="cell100 column4">SERIAL NUMBER</th>
+                      <th className="cell100 column4">Serial Number</th>
                       <th className="cell100 column5">Remove</th>
                     </tr>
                   </thead>
@@ -84,4 +93,11 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+Posts.propTypes = {
+  // education: PropTypes.array.isRequired,
+  deleteProfile: PropTypes.func.isRequired
+};
+
+export default connect(null, 
+  { deleteProfile }
+)(Posts);
