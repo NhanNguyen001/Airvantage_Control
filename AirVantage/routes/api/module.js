a6f2eb569544bb4c7a569efef9bd5ed882f7fa4a
NhanNguyen001 <nhan2151@gmail.com>
2019-08-28 00:28:10 +0700

    restructure fomat of data before sending to client

--- AirVantage/routes/api/module.js
+++ AirVantage/routes/api/module.js
@@ -3,7 +3,10 @@ const request = require('request');
 const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const { check, validationResult } = require('express-validator/check');
+const {
+	check,
+	validationResult
+} = require('express-validator/check');
 
 const Module = require('../../models/Module');
 const User = require('../../models/User');
@@ -13,17 +16,19 @@ const User = require('../../models/User');
 // @access    Private
 router.get('/', auth, async (req, res) => {
 	try {
-			const module = await Module.find()
-						
-			if(!module){
-					res.status(400).json({ msg: 'There is no module in database' });
-			}
-			
-			res.json(module);
+		const module = await Module.find()
+
+		if (!module) {
+			res.status(400).json({
+				msg: 'There is no module in database'
+			});
+		}
+
+		res.json(module);
 
 	} catch (err) {
-			console.error(err.message);
-			res.status(500).send('Server Error');
+		console.error(err.message);
+		res.status(500).send('Server Error');
 	}
 });
 
@@ -32,17 +37,24 @@ router.get('/', auth, async (req, res) => {
 // @access    Private
 router.get('/user/:userID', auth, async (req, res) => {
 	try {
-		const module = await Module.find({ user: req.user.id }).populate('user');
+		const module = await Module.find({
+			user: req.user.id
+		}).populate('user');
 
-		if(!module){
-				res.status(400).json({ msg: 'There is no module profile for this user' });
+		if (!module) {
+			res.status(400).json({
+				msg: 'There is no module profile for this user'
+			});
 		}
-		
-		res.json(module);
+
+		res.json({
+			size: module.length,
+			items: module
+		});
 
 	} catch (err) {
-			console.error(err.message);
-			res.status(500).send('Server Error');
+		console.error(err.message);
+		res.status(500).send('Server Error');
 	}
 });
 
@@ -51,65 +63,78 @@ router.get('/user/:userID', auth, async (req, res) => {
 // @access    Private
 router.get('/:id', auth, async (req, res) => {
 	try {
-			const module = await Module.findById(req.params.id);
-	
-			if (!module) {
-				return res.status(404).json({ msg: 'Module not found' });
-			}
-	
-			res.json(module);
+		const module = await Module.findById(req.params.id);
+
+		if (!module) {
+			return res.status(404).json({
+				msg: 'Module not found'
+			});
+		}
+
+		res.json(module);
 	} catch (err) {
-			if (err.kind === 'ObjectId') {
-				return res.status(404).json({ msg: 'Module not found' });
-			}
-			res.status(500).send('Server Error');
+		if (err.kind === 'ObjectId') {
+			return res.status(404).json({
+				msg: 'Module not found'
+			});
+		}
+		res.status(500).send('Server Error');
 	}
 });
 
 // @route     POST api/module
 // @desc      Create or update user module profile
 // @access    Private
-router.post( '/', 
-	[ auth, [   
-			check('name', 'Name is required')
-				.not()
-				.isEmpty(),
-			check('serialnumber', 'SerialNumber is required')
-				.not()
-				.isEmpty(),
-			// check('imei', 'IMEI is required')
-			// 	.not()
-			// 	.isEmpty(),
-			// check('macaddress', 'MACaddress is required')
-			// 	.not()
-			// 	.isEmpty(),
-		]
-	], async (req, res) => {
-			const errors = validationResult(req);
-			if(!errors.isEmpty()){
-				return res.status(400).json({ errors: errors.array() });
-			}
-			const {
-				name,
-				serialnumber,
-				imei,
-				macaddress
-			} = req.body;
+router.post('/',
+	[auth, [
+		check('name', 'Name is required')
+		.not()
+		.isEmpty(),
+		check('serialnumber', 'SerialNumber is required')
+		.not()
+		.isEmpty(),
+		// check('imei', 'IMEI is required')
+		// 	.not()
+		// 	.isEmpty(),
+		// check('macaddress', 'MACaddress is required')
+		// 	.not()
+		// 	.isEmpty(),
+	]], async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({
+				errors: errors.array()
+			});
+		}
+		const {
+			name,
+			serialnumber,
+			imei,
+			macaddress
+		} = req.body;
 
-			// Build module profile object
-			const moduleFields = {};
-			moduleFields.user = req.user.id;
-			if(name) moduleFields.name = name;
-			if(serialnumber) moduleFields.serialnumber = serialnumber;
-			if(imei) moduleFields.imei = imei;
-			if(macaddress) moduleFields.macaddress = macaddress;
+		// Build module profile object
+		const moduleFields = {};
+		moduleFields.user = req.user.id;
+		if (name) moduleFields.name = name;
+		if (serialnumber) moduleFields.serialnumber = serialnumber;
+		if (imei) moduleFields.imei = imei;
+		if (macaddress) moduleFields.macaddress = macaddress;
 		try {
-			let checkModuleName = await Module.findOne({ name: moduleFields.name });
-			let checkModuleSerialNumber = await Module.findOne({ serialnumber: moduleFields.serialnumber });
-			if(checkModuleName || checkModuleSerialNumber){
-				return res.status(400).json({ msg: 'This module is available, please enter new module' });
+			let checkModuleName = await Module.findOne({
+				name: moduleFields.name
+			});
+			let checkModuleSerialNumber = await Module.findOne({
+				serialnumber: moduleFields.serialnumber
+			});
+			if (checkModuleName || checkModuleSerialNumber) {
+				return res.status(400).json({
+					msg: 'This module is available, please enter new module'
+				});
 			}
-			let module = await Module.findOne({ user: req.user.id });
+			let module = await Module.findOne({
+				user: req.user.id
+			});
 			module = new Module(moduleFields);
 			await module.save();
 			res.json(module);
@@ -124,7 +149,7 @@ router.post( '/',
 // @route     POST api/module
 // @desc      Update module profile
 // @access    Private
-router.post( '/:id', auth, async (req, res) => {
+router.post('/:id', auth, async (req, res) => {
 	const moduleFieldsUpdate = {};
 	try {
 		await Module.findById(req.params.id);
@@ -136,12 +161,14 @@ router.post( '/:id', auth, async (req, res) => {
 		} = req.body;
 		// Build module profile object
 		moduleFieldsUpdate.user = req.user.id;
-		if(name) moduleFieldsUpdate.name = name;
-		if(serialnumber) moduleFieldsUpdate.serialnumber = serialnumber;
-		if(imei) moduleFieldsUpdate.imei = imei;
-		if(macaddress) moduleFieldsUpdate.macaddress = macaddress;
+		if (name) moduleFieldsUpdate.name = name;
+		if (serialnumber) moduleFieldsUpdate.serialnumber = serialnumber;
+		if (imei) moduleFieldsUpdate.imei = imei;
+		if (macaddress) moduleFieldsUpdate.macaddress = macaddress;
 	} catch (error) {
-		return res.status(404).json({ msg: 'Module not found' });
+		return res.status(404).json({
+			msg: 'Module not found'
+		});
 	}
 
 	try {
@@ -153,31 +180,34 @@ router.post( '/:id', auth, async (req, res) => {
 		// if(checkModuleName || checkModuleSerialNumber || checkModuleImei || checkModuleMacaddress){
 		// 	return res.status(400).json({ msg: 'This module is available, please enter new module' });
 		// }
-		let module = await Module.findOneAndUpdate(
-			{ _id: req.params.id },
-			{ $set: moduleFieldsUpdate },
-			{ new: true }
-		);
+		let module = await Module.findOneAndUpdate({
+			_id: req.params.id
+		}, {
+			$set: moduleFieldsUpdate
+		}, {
+			new: true
+		});
 		res.json(module);
 	} catch (error) {
-			console.error(error.message);
-			res.status(500).send('Server Error')
-		}
+		console.error(error.message);
+		res.status(500).send('Server Error')
 	}
-);
+});
 
 // @route     DELETE api/module
 // @desc      Delete profile, user and posts
 // @access    Private
-router.delete('/:id', auth, async (req, res) =>{
+router.delete('/:id', auth, async (req, res) => {
 	try {
 		// Remove module by ID
-		await Module.findOneAndRemove({ _id: req.params.id });
+		await Module.findOneAndRemove({
+			_id: req.params.id
+		});
 		let module = await Module.find()
 		res.json(module);
 	} catch (err) {
-			console.error(err.message);
-			res.status('500').send('Server Error');
+		console.error(err.message);
+		res.status('500').send('Server Error');
 	}
 });
 
